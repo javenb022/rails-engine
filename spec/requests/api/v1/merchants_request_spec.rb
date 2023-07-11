ef9890ed2dcf9177f88739626ea0ad29e05871c3
Javen Wilson <javenb022@gmail.com>
2023-07-11 16:54:58 -0400

    Hardens test for merchants items

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -51,7 +51,23 @@ RSpec.describe "Merchants API" do
       expect(response).to be_successful
 
       merchant_items = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+
+      merchant_items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id].to_i).to be_an(Integer)
+
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      end
     end
   end
 end

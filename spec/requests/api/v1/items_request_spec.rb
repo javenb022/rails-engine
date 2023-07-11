a4f3467243f25206ba207163ebefb4c135b92d05
Javen Wilson <javenb022@gmail.com>
2023-07-11 18:02:30 -0400

    Test: returns a single item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -30,4 +30,39 @@ RSpec.describe "Items API" do
       end
     end
   end
+
+  describe "GET /api/v1/items/:id" do
+    it "returns a single item" do
+      merchant = create(:merchant)
+      item = create(:item, merchant_id: merchant.id)
+      item2 = create(:item, merchant_id: merchant.id)
+
+      get "/api/v1/items/#{item.id}"
+
+      expect(response).to be_successful
+
+      item_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item_response[:data]).to have_key(:id)
+      expect(item_response[:data][:id].to_i).to be_an(Integer)
+
+      expect(item_response[:data][:attributes]).to have_key(:name)
+      expect(item_response[:data][:attributes][:name]).to be_a(String)
+
+      expect(item_response[:data][:attributes]).to have_key(:description)
+      expect(item_response[:data][:attributes][:description]).to be_a(String)
+
+      expect(item_response[:data][:attributes]).to have_key(:unit_price)
+      expect(item_response[:data][:attributes][:unit_price]).to be_a(Float)
+
+      expect(item_response[:data][:attributes]).to have_key(:merchant_id)
+      expect(item_response[:data][:attributes][:merchant_id]).to be_an(Integer)
+    end
+  end
+
+  describe "POST /api/v1/items" do
+    it "creates an item" do
+      
+    end
+  end
 end

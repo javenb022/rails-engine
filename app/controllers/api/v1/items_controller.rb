7286e2e501ee14878ee9f928c0712dfa13209947
Javen Wilson <javenb022@gmail.com>
2023-07-11 17:37:19 -0400

    Feat: Index action for items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: ItemSerializer.new(Merchant.find(params[:merchant_id]).items)
+    render json: ItemSerializer.new(Item.all)
   end
+
+  # def show
+  #   render json: MerchantSerializer.new(Item.find(params[:id]).merchant)
+  # end
 end

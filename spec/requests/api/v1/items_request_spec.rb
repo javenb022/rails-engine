bc225448e94183c3126fff14eb5784d11b9f398b
Javen Wilson <javenb022@gmail.com>
2023-07-11 23:48:15 -0400

    Test: happy and sad path for post

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -62,6 +62,130 @@ RSpec.describe "Items API" do
 
   describe "POST /api/v1/items" do
     it "creates an item" do
+      merchant = create(:merchant)
+
+      post "/api/v1/items", params: {
+        name: "Soap",
+        description: "Cleans your body well",
+        unit_price: 10.99,
+        merchant_id: "#{merchant.id}"
+      }
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      item_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item_response[:data]).to have_key(:id)
+      expect(item_response[:data][:id].to_i).to be_an(Integer)
+
+      expect(item_response[:data][:attributes]).to have_key(:name)
+      expect(item_response[:data][:attributes][:name]).to be_a(String)
+      expect(item_response[:data][:attributes][:name]).to eq("Soap")
+
+      expect(item_response[:data][:attributes]).to have_key(:description)
+      expect(item_response[:data][:attributes][:description]).to be_a(String)
+      expect(item_response[:data][:attributes][:description]).to eq("Cleans your body well")
+
+      expect(item_response[:data][:attributes]).to have_key(:unit_price)
+      expect(item_response[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(item_response[:data][:attributes][:unit_price]).to eq(10.99)
+
+      expect(item_response[:data][:attributes]).to have_key(:merchant_id)
+      expect(item_response[:data][:attributes][:merchant_id]).to be_an(Integer)
+    end
+
+    it "returns an error if any attributes are missing" do
+      merchant = create(:merchant)
+
+      post "/api/v1/items", params: {
+        # name: "Soap",
+        # description: "Cleans your body well",
+        unit_price: 10.99,
+        merchant_id: "#{merchant.id}"
+      }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:error]).to eq("Name can't be blank and Description can't be blank")
+    end
+
+    it "ignores any attributes that are not allowed" do
+      merchant = create(:merchant)
+
+      post "/api/v1/items", params: {
+        name: "Soap",
+        description: "Cleans your body well",
+        unit_price: 10.99,
+        merchant_id: "#{merchant.id}",
+        color: "blue"
+      }
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      item_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item_response[:data]).to have_key(:id)
+      expect(item_response[:data][:id].to_i).to be_an(Integer)
+
+      expect(item_response[:data][:attributes]).to have_key(:name)
+      expect(item_response[:data][:attributes][:name]).to be_a(String)
+      expect(item_response[:data][:attributes][:name]).to eq("Soap")
+
+      expect(item_response[:data][:attributes]).to have_key(:description)
+      expect(item_response[:data][:attributes][:description]).to be_a(String)
+      expect(item_response[:data][:attributes][:description]).to eq("Cleans your body well")
+
+      expect(item_response[:data][:attributes]).to have_key(:unit_price)
+      expect(item_response[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(item_response[:data][:attributes][:unit_price]).to eq(10.99)
+
+      expect(item_response[:data][:attributes]).to have_key(:merchant_id)
+      expect(item_response[:data][:attributes][:merchant_id]).to be_an(Integer)
+
+      expect(item_response[:data][:attributes]).to_not have_key(:color)
+    end
+  end
+
+  describe "PATCH /api/v1/items/:id" do
+    it "updates an item" do
+      merchant = create(:merchant)
+      item = merchant.items.create(name: "Soap", description: "Cleans your body well", unit_price: 10.99)
+
+      patch "/api/v1/items/#{item.id}", params: {
+        description: "Cleans your body well and smells good"
+      }
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data]).to have_key(:id)
+      expect(json[:data][:id].to_i).to be_an(Integer)
+
+      expect(json[:data][:attributes]).to have_key(:name)
+      expect(json[:data][:attributes][:name]).to be_a(String)
+      expect(json[:data][:attributes][:name]).to eq("Soap")
+
+      expect(json[:data][:attributes]).to have_key(:description)
+      expect(json[:data][:attributes][:description]).to be_a(String)
+      expect(json[:data][:attributes][:description]).to eq("Cleans your body well and smells good")
+      expect(json[:data][:attributes][:description]).to_not eq("Cleans your body well")
+
+      expect(json[:data][:attributes]).to have_key(:unit_price)
+      expect(json[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(json[:data][:attributes][:unit_price]).to eq(10.99)
+
+      expect(json[:data][:attributes]).to have_key(:merchant_id)
+      expect(json[:data][:attributes][:merchant_id]).to be_an(Integer)
+    end
+
+    it "returns an error if the merchant id is invalid" do
       
     end
   end

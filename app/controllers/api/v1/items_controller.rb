1687acc02cf5ee206f1abaf04ebd4fd71758d5a1
Javen Wilson <javenb022@gmail.com>
2023-07-11 23:47:29 -0400

    Feat: create and update actions

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,28 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    new_item = Item.create(item_params)
+    if new_item.save
+      render json: ItemSerializer.new(new_item), status: :created #location: api_v1_item_path(new_item)
+    else
+      render json: { error: new_item.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: ItemSerializer.new(item)
+    else
+      render json: { error: item.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
 end

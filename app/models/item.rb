ef50db2befc0857ff4f604bc374c2e052b4bc692
Javen Wilson <javenb022@gmail.com>
2023-07-11 23:47:39 -0400

    Add validations to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   # has_many :invoice_items
   # has_many :invoices, through: :invoice_items
   # has_many :transactions, through: :invoices
+  validates :name, :description, :unit_price, presence: true, allow_blank: false
 end

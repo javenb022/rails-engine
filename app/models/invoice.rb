06f29d9c5f44ed33a7e2888cbb58c75c9d14fe12
Javen Wilson <javenb022@gmail.com>
2023-07-14 15:37:38 -0400

    Feat: Add transaction association

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,7 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :delete_all
   has_many :items, through: :invoice_items
+  has_many :transactions
 
   validates :status, :customer_id, :merchant_id, presence: true # , allow_blank: false
 end
